7ec0703ace8acc13bf7f4b28d290f30cef239560
Utkarsh Kumar <utkarshkumar965@gmail.com>
2020-12-29 17:59:48 +0530

    Updated Javascript

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,26 @@
 let screen=document.getElementById('screen');
+let buttons=document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        if(buttonText=='X'){
+            buttonText='*';
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+        else if(buttonText=='C'){
+            screenValue='';
+            screen.value=screenValue;
+        }
+        else if(buttonText=='='){
+            screenValue=eval(screenValue);
+            screen.value=screenValue;
+        }
+        else
+        {
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+    })
+}
